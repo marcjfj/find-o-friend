d3dc58c73cb5392705c3d3bbb157d18ceeb5e3ce
Marc <marcjfj@gmail.com>
2019-12-07 18:23:28 -0600

    removed stuff

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
