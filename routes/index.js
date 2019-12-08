96ddc4f580904f6a4c6b6344cd0b622cc702822b
Marc <marcjfj@gmail.com>
2019-12-07 18:15:42 -0600

    removed stuff

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const questions = q.questions();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 /* GET quiz page. */
